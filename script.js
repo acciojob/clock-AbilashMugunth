b67083aed1f5b59c0bedf2a4c2d99402f29d872d
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-07 19:18:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let timerDisplay = document.querySelector("#timer");
+let day = new Date();
+timerDisplay.innerText = `${day.toLocaleDateString()}, ${day.toLocaleTimeString()}`;
+
+function time() {
+  setInterval(() => {
+    let day = new Date();
+
+    console.log(day.toLocaleDateString());
+    console.log(day.toLocaleTimeString());
+
+    timerDisplay.innerText = `${day.toLocaleDateString()}, ${day.toLocaleTimeString()}`;
+  }, 1000);
+}
+
+time();
